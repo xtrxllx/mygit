805d1a0532232c37eaee004baf9ee2f0f90879d7
xtrxllx <shazwanimizainiK15@gmail.com>
2020-10-27 12:53:30 -0700

    edited commit

--- day1.1.py
+++ day1.1.py
@@ -1,5 +1,5 @@
 age = int(input('Enter your age: '))
 max_heart_rate = 206.9 - (0.67 * age)
-target. = 0.6 * max_heart_rate
-print ('Your target fat burning heart rate is: ', target.)
+target = 0.6 * max_heart_rate
+print ('Your target fat burning heart rate is: ', target)
 
--- day1.py
+++ day1.py
@@ -1,6 +1,6 @@
 import math
 
-radius_str = input("Enter the radius of your circle: ")
+radius_str = input("Enter the radius of the circle: ")
 radius_int = int(radius_str)
 
 circumference = 2*math.pi*radius_int
