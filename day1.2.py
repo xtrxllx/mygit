c5f0e6a4f5e460778876a8ddc0288e631135cf18
xtrxllx <shazwanimizainiK15@gmail.com>
2020-10-27 12:38:09 -0700

    edited

--- day1.2.py
+++ day1.2.py
@@ -1,4 +1,4 @@
 age = int(input("Enter your age in years: "))
 max_heart_rate = 206.9 - (0.67*age)
-target = 0.65*max_heart_rate
-print("Target fat-burning heart rate is:", target)
+target. = 0.65*max_heart_rate
+print("Target fat-burning heart rate is:", target.)
